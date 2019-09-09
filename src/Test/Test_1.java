d73660546d290beb24c187622ac47b6190ef62a1
hyk0116 <42903536+hyk0116@users.noreply.github.com>
2019-09-09 20:53:52 +0900

    백준 2885

--- src/Test/Test_1.java
+++ src/Test/Test_1.java
@@ -3,15 +3,18 @@ import java.util.Scanner;
 public class Test_1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// น้มุ 2885
 		Scanner sc= new Scanner(System.in);
 		
 		int n1=sc.nextInt();
 		int n2=sc.nextInt();
-		//int mul1=
-		System.out.println(n1*((n2%100)%10));
-		System.out.println(n1*((n2%100)/10));
-		System.out.println(n1*(n2/100));
+		int result1=n1*((n2%100)%10);
+		int result2=n1*((n2%100)/10);
+		int result3=n1*(n2/100);
+		System.out.println(result1);
+		System.out.println(result2);
+		System.out.println(result3);
+		System.out.println(result1+result2*10+result3*100);
 	}
 
 }
