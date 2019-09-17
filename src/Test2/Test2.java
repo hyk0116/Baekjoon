66e7ba33d8904f52a1b0a08f205e37840b209267
hyk0116 <42903536+hyk0116@users.noreply.github.com>
2019-09-17 22:05:11 +0900

    과정2

--- src/Test2/Test2.java
+++ src/Test2/Test2.java
@@ -6,6 +6,10 @@ public class Test2 {
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
 		String s=sc.next();
+		String[] arr;
+		int count=0;
+		
+		StringBuffer strbf=new StringBuffer(s);//문자열 변경위해 사용
 		
 		System.out.println(s);
 	}
