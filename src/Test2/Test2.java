b7767375c4c704449778fa7ada7fad68b4411c72
hyk0116 <42903536+hyk0116@users.noreply.github.com>
2019-09-17 22:40:56 +0900

    구현과제 -최종

--- src/Test2/Test2.java
+++ src/Test2/Test2.java
@@ -9,21 +9,33 @@ public class Test2 {
 		String s=sc.next();
 		String[] arr;
 		int count=0;
-		
+
 		StringBuffer strbf=new StringBuffer(s);
-		
+
 		for(int i=1; i<s.length(); i++) {
 			if(s.charAt(i)!=s.charAt(i-1)) {
-				strbf.insert(count, " ");
+				strbf.insert(i+count, " ");
 				count++;
 			}
 		}
 
-		System.out.println(s);
+		arr=strbf.toString().split(" ");
+
+		char c;
+		int count2;
+
+		for(int i=0; i<arr.length;i++) {
+			count2=arr[i].length();
+			c=arr[i].charAt(0);
+			
+			if(count2==1) {
+				System.out.print(c);
+			}else {
+				System.out.print(count2+""+c);
+			}
+		}
 
 
-		
-		
 	}
 
 }
