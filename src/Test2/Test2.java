515c8d42f5b4a28b797d13387ab3a0c70fa44844
hyk0116 <42903536+hyk0116@users.noreply.github.com>
2019-09-17 21:58:12 +0900

    구현과제1-과정1

--- src/Test2/Test2.java
+++ src/Test2/Test2.java
@@ -1,10 +1,13 @@
 package Test2;
-
+import java.util.Scanner;
 public class Test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		
+		System.out.println(s);
 	}
 
 }
