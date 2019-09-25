a10c5ef86a98658af277972ccea4099ade9644db
hyk0116 <42903536+hyk0116@users.noreply.github.com>
2019-09-25 18:11:42 +0900

    소수찾기-과정1

---
+++ src/Test3/Test3.java
@@ -0,0 +1,26 @@
+package Test3;
+import java.util.Scanner;
+import java.util.Arrays;
+public class Test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//1978
+		Scanner sc=new Scanner(System.in);
+		
+		
+		int a=sc.nextInt();
+		int [] arr=new int [a];
+		int count =0;
+		
+		for(int i=0; i<arr.length; i++) {
+			arr[i]=sc.nextInt();
+
+
+		}
+	
+		
+		
+	}
+
+}
