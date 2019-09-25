985840b1904260bfee0be8ccf287b34af2f2045a
hyk0116 <42903536+hyk0116@users.noreply.github.com>
2019-09-25 18:30:52 +0900

    최종결과

--- src/Test3/Test3.java
+++ src/Test3/Test3.java
@@ -10,7 +10,7 @@ public class Test3 {
 		
 		
 		int a=sc.nextInt();
-		int [] arr=new int [a];
+		int arr[] =new int [a];
 		boolean b=true;//소수여부 판단
 		int count =0;
 		
@@ -18,16 +18,19 @@ public class Test3 {
 			arr[i]=sc.nextInt();
 			
 			if(arr[i]<=1)
-				b=false;
+				b=false;//0이나 1일때 
 			else {
 				for(int j=2;j<arr[i]/2; j++)
-					if(arr[i]/j==0)
-					b=false;
+					if(arr[i]%j==0)
+					b=false;//소수가 아닐때
 			}
+			if(b)
+				count++;
+			b=true;
 			
 		}
 	
-		
+		System.out.println(count);
 			
 	}
 
