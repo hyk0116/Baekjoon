a7fb2daa8335e4cec7008b96f5100c4d25abc150
hyk0116 <42903536+hyk0116@users.noreply.github.com>
2019-10-04 21:50:50 +0900

    백준4673

--- src/Test4/Test4.java
+++ src/Test4/Test4.java
@@ -5,7 +5,11 @@ public class Test4 {
 	public static void main(String[] args) {
 		boolean notSelfNum[]=new boolean [10001];//셀프넘버가 아닌 수를 저장하는 불린 배열
 
-		
+		for(int i=1; i<=10000;i++) {
+			int dn=dn(i);
+			if(dn<=10000)
+				notSelfNum[dn]=true;
+		}
 		for(int i=0; i<notSelfNum.length;i++) {
 			if(!notSelfNum[i]) {
 				System.out.println(i);
